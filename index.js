5d52cc46c5697512ec9b0138b6e672a139e81e58
vinay <vinaysunku5499@gmail.com>
2019-12-20 15:30:36 +0530

    chage for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static('frontend'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
